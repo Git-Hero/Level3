739001c43b87fe3eec8a83241a5119816dc3d446
ankushjangid7 <iit2014119@iiita.ac.in>
2016-04-04 00:52:06 +0530

    Update md5gen.sh

--- md5gen.sh
+++ md5gen.sh
@@ -1,4 +1,3 @@
-#!/usr/bin/bash
 # md5gen.sh : Bash script (not sh)
 # Script to generate md5sum of a given number of strings and append them to a file "md5file"
 # Useful when you are hosting some quiz and want the answers to be hashed.
@@ -7,28 +6,29 @@
 
 read  -p "Enter number of strings: " n
 
-$fname='md5file'
+$fname="md5file";
 
-read -p "Enter any remarks for this record : " rem
+read -p "Enter any remarks for this record : " rem ;
 
 if [ ! -f "$fname" ]; then
 	read -p "File does not exist ! Create new file? (y/n) : " $choice
-	if [[ $choice = 'y' ]]; then
-		touch "$fname"
+	if [ "$choice" = 'y' ]; then
+		touch "$fname";
 	else
-		exit 1
+		exit 1;
+        fi		
 
 fi
 echo "$rem" >> $fname
-i=0
+i=0;
 while [[ $i -lt $n ]];
 	do
 		read -p "$($i+1) " ip
 		echo -n "$($i+1) " >> $fname
 		echo -n "$ip" > tempfile
 		md5sum tempfile | cut -f1 -d' '  > $fname
-		i=(($i+1))
+		i=($i+1)
 	done
 
 echo "" > $fname
-rm $tempfile
+rm $tempfile;
