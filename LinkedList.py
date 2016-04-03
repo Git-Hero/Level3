44cf5faf6dbfc4353af09a0220a6090dbdf37a4f
ankushjangid7 <iit2014119@iiita.ac.in>
2016-04-03 23:44:32 +0530

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 
 # Linked list implementation in python (python 3)
-#include <iostream>
-using namespace std;
 
 class Node:
 	def __init__ (self, data):
 		self.data = data
-		self = None
+		self.next= None
 
 
 class LinkedList:
@@ -18,13 +16,14 @@ class LinkedList:
 		tmp = self.head
 		ctr = 0
 		while (tmp != None):
-			self.head = tmp.next
+			tmp = tmp.next
+			ctr=ctr+1
 		return ctr
 	
 	def display (self):
 		tmp = self.head
 		while (tmp != None):
-			printf(tmp.data, end = ' ');
+			print(tmp.data);
 			tmp = tmp.next
 		return None
 
@@ -33,45 +32,44 @@ class LinkedList:
 		new_node.next = self.head
 		self.head = new_node
 	
-	def delAtBeg (self)
+	def delAtBeg (self):
 		if (self.head is None):
 			print ("List is already empty!");
 			return None
 		val = self.head.data
-		self.head = self.head.next.next
+		self.head = self.head.next
 	
-	def insertAtEnd (data):
+	def insertAtEnd (self,data):
 		if (self.head is None):
 			new_node = Node(data)
-			new_node.next = NULL
+			new_node.next = None
 			self.head = new_node
 		tmp = self.head
 		while (tmp.next is not None):
 			tmp = tmp.next
 		tmp.next = Node(data)
 	
-	def delAtEnd (self, data):
-		if (!(self.head is None);
+	def delAtEnd (self):
+		if (self.head is None):
 			print ("List is empty!")
 			return None
 		if (self.head.next is None):
 			del self.head
 			self.head = None
+			return None
 		tmp = self.head
 		while (tmp.next.next != None):
 			tmp = tmp.next
 		var = tmp.next.data
 		del tmp.next
 		tmp.next = None
-		return var
 
 
-
-l = linkedlist()
+l = LinkedList()
 l.insertAtBeg(10)
 l.insertAtEnd(100)
 l.insertAtBeg(1000)
-l.delAtBeg(10)
+l.delAtBeg()
 l.delAtEnd()
-l.count()
+print(l.count())
 l.display()
